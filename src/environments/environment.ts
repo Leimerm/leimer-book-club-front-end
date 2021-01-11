0d7c84dfbe5a3517a5d1ba98eaa8e3c3e4e45fd7
Matthew Leimer <matthewleimer@gmail.com>
2021-01-10 21:21:31 -0600

    Heroku Push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://leimer-book-club-api.herokuapp.com/'
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://leimer-book-club-api.herokuapp.com/'
+  // apiUrl: 'http://localhost:3000'
 };
 
 /*
