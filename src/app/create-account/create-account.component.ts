1d05832652ca432867015f7e72de8949111bb893
Matthew Leimer <matthewleimer@gmail.com>
2021-01-13 17:52:45 -0600

    Can create new users

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  newUserInfo: FormGroup = new FormGroup({
+    user_name: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required]),
+    password_confirmation: new FormControl("", [Validators.required]),
+  });
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  addNewUser() {
+    this.userService.createUser(this.newUserInfo.value).subscribe(data=>{
+    console.log(data);
+    this.router.navigate([`/book-list`]);
+    })
+  }
+
+  clearUserInfo() {
+    this.newUserInfo.reset(
+    {userName: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+    }
+    )
+  }
 }
